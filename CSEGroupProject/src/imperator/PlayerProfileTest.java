dc1284a066a29a48a8b3371cf6409343fb85ef41
inlandquarter <inlandquarter@fabians-mbp.mobile.asu.edu>
2016-04-15 15:48:22 -0700

    Adding PlayerProfile junit test

---
+++ CSEGroupProject/src/imperator/PlayerProfileTest.java
@@ -0,0 +1,14 @@
+package imperator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayerProfileTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
