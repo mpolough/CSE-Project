f0deccf7de6c022a4fc3e3ba16a03989e92b3379
inlandquarter <inlandquarter@fabians-mbp.mobile.asu.edu>
2016-04-15 16:43:51 -0700

    PlayerProfileTest implemented

--- CSEGroupProject/src/imperator/LeaderboardTest.java
+++ CSEGroupProject/src/imperator/LeaderboardTest.java
@@ -7,15 +7,38 @@ import org.junit.Test;
 public class LeaderboardTest {
 
 	@Test
-	public void reader() {
-		
+	public void testReader() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
-	public void writer(){
-		fail("nope");
+	public void testMonitor() {
+		fail("Not yet implemented");
 	}
 
-	
+	@Test
+	public void testGetProfile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWriter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddNewProfile() {
+		fail("Not yet implemented");
+	}
+
 }
--- CSEGroupProject/src/imperator/PlayerProfileTest.java
+++ CSEGroupProject/src/imperator/PlayerProfileTest.java
@@ -7,8 +7,116 @@ import org.junit.Test;
 public class PlayerProfileTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testPlayerProfile() {
+		PlayerProfile profile = new PlayerProfile();
+		assertEquals("NULL", profile.getPlayerName());
+		assertNotNull("NULL", profile.getPlayerClass());
+		assertEquals(0, profile.getNumKills());
+		assertEquals(0, profile.getNumDeaths());
+		assertEquals(0.0, profile.getMovesPerGame(), 0);
+		assertEquals(0.0, profile.getDamageDonePerGame(), 0);
+		assertEquals(0, profile.getGamesPlayed());	
+	}
+
+	@Test
+	public void testPlayerProfileStringString() {
+		PlayerProfile profile = new PlayerProfile("Name", "Class");
+		assertEquals("Name", profile.getPlayerName());
+		assertNotNull("Class", profile.getPlayerClass());
+		assertEquals(0, profile.getNumKills());
+		assertEquals(0, profile.getNumDeaths());
+		assertEquals(0.0, profile.getMovesPerGame(), 0);
+		assertEquals(0.0, profile.getDamageDonePerGame(), 0);
+		assertEquals(0, profile.getGamesPlayed());	
+	}
+
+	@Test
+	public void testGetPlayerName() {
+		PlayerProfile profile = new PlayerProfile("Name", "Class");
+		assertEquals("Name", profile.getPlayerName());	
+	}
+
+	@Test
+	public void testSetPlayerName() {
+		PlayerProfile profile = new PlayerProfile("Name", "Class");
+		profile.setPlayerName("Test");
+		assertEquals("Test", profile.getPlayerName());	
+	}
+
+	@Test
+	public void testGetPlayerClass() {
+		PlayerProfile profile = new PlayerProfile("Name", "Class");
+		assertEquals("Class", profile.getPlayerClass());		}
+
+	@Test
+	public void testSetPlayerClass() {
+		PlayerProfile profile = new PlayerProfile("Name", "Class");
+		profile.setPlayerClass("Test");
+		assertEquals("Test", profile.getPlayerClass());	
+	}
+
+	@Test
+	public void testGetNumKills() {
+		PlayerProfile profile = new PlayerProfile("Name", "Class");
+		assertEquals(0, profile.getNumKills());
+	}
+
+	@Test
+	public void testSetNumKills() {
+		PlayerProfile profile = new PlayerProfile("Name", "Class");
+		profile.setNumKills(1);
+		assertEquals(1, profile.getNumKills());
+	}
+
+	@Test
+	public void testGetNumDeaths() {
+		PlayerProfile profile = new PlayerProfile("Name", "Class");
+		assertEquals(0, profile.getNumDeaths());
+	}
+
+	@Test
+	public void testSetNumDeaths() {
+		PlayerProfile profile = new PlayerProfile("Name", "Class");
+		profile.setNumDeaths(1);
+		assertEquals(1, profile.getNumDeaths());	}
+
+	@Test
+	public void testGetMovesPerGame() {
+		PlayerProfile profile = new PlayerProfile("Name", "Class");
+		assertEquals(0.0, profile.getMovesPerGame(), 0);
+	}
+
+	@Test
+	public void testSetMovesPerGame() {
+		PlayerProfile profile = new PlayerProfile("Name", "Class");
+		profile.setMovesPerGame(1.0);
+		assertEquals(1.0, profile.getMovesPerGame(), 0);
+	}
+
+	@Test
+	public void testGetDamageDonePerGame() {
+		PlayerProfile profile = new PlayerProfile("Name", "Class");
+		assertEquals(0.0, profile.getDamageDonePerGame(),0);
+	}
+
+	@Test
+	public void testSetDamageDonePerGame() {
+		PlayerProfile profile = new PlayerProfile("Name", "Class");
+		profile.setDamageDonePerGame(1.0);
+		assertEquals(1.0, profile.getDamageDonePerGame(), 0);
+	}
+
+	@Test
+	public void testGetGamesPlayed() {
+		PlayerProfile profile = new PlayerProfile("Name", "Class");
+		assertEquals(0, profile.getGamesPlayed());
+	}
+
+	@Test
+	public void testSetGamesPlayed() {
+		PlayerProfile profile = new PlayerProfile("Name", "Class");
+		profile.setGamesPlayed(1);
+		assertEquals(1, profile.getGamesPlayed());
 	}
 
 }
