e2e2183f208d120604935edcc8b1e4cc11c58979
inlandquarter <inlandquarter@fabians-mbp.mobile.asu.edu>
2016-04-15 02:20:15 -0700

    removed comments

--- CSEGroupProject/src/imperator/Leaderboard.java
+++ CSEGroupProject/src/imperator/Leaderboard.java
@@ -137,12 +137,4 @@ public class Leaderboard {
 	
 	//write sort() methods to sort the profile array on one of the stats, print that sorted array (leaderboard)
 	
-	/* Overall should work as follows: read in the leaderboard text file and create the 
-	profile object array. When a game is played the player enters the name of the profile to use, 
-	or creates a new profile. If a new profile is created, add a new profile object to array.
-	If the player selects an existing profile, keep track of the array position of that profile 
-	object for updating at game end.  
-	While the game is played, the game will keep track of stats to update. At the end of the game, 
-	update() will automatically change the stat fields, and update the text file for later use. 
-	*/
 }
