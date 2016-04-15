5b6305074b8d6cafe8aab4d4bdc5fad9d95085cd
inlandquarter <inlandquarter@fabians-mbp.mobile.asu.edu>
2016-04-15 15:47:09 -0700

    Updating unit test

---
+++ CSEGroupProject/src/imperator/LeaderboardTest.java
@@ -0,0 +1,21 @@
+package imperator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LeaderboardTest {
+
+	@Test
+	public void reader() {
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void writer(){
+		fail("nope");
+	}
+
+	
+}
