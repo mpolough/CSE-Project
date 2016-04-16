d849a427630addbf95cb05419cd945050d712afc
Chris Ward <caesartopher@gmail.com>
2016-04-16 01:18:36 -0700

    Comments

--- CSEGroupProject/src/imperator/LeaderboardTest.java
+++ CSEGroupProject/src/imperator/LeaderboardTest.java
@@ -9,15 +9,13 @@ import org.junit.Test;
 public class LeaderboardTest {
 
 	@Test
-	public void testReader() {
-		Leaderboard obj = new Leaderboard();
-//		try {
-//			//obj.reader();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		fail("Not yet implemented");
+	public void testReader() throws IOException {
+		PlayerProfile profile = new PlayerProfile("Joe", "Mage");
+		Leaderboard board = new Leaderboard();
+		board.addNewProfile(profile);
+		board.writer();
+		board.reader();
+		assertEquals(profile, board.getProfile(profile.getPlayerName()));
 	}
 
 	@Test
