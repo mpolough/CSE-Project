1e866f170fd6d9966502e5a8ac54d58ae251607a
Selene <selenemoraga18@gmail.com>
2016-04-29 13:32:04 -0700

    Added files via upload

--- Battlefield.java
+++ Battlefield.java
@@ -20,9 +20,9 @@ public class Battlefield {
 		player1Position = 1;
 		player2Position = baseSize;
 		base = new String [baseSize + 2];
-		playerBase = new String [baseSize +2];
+		playerBase = new String [baseSize + 2];
 	}
-	public void initializeFloor()
+	public void initializeFloor ()
 	{
 		//Creates battle field floor
 		for(int index = 0; index < base.length ; index ++)
@@ -33,6 +33,25 @@ public class Battlefield {
 	public void initializePlayerFloor()
 	{
 		//Sets player start position
+		playerBase[player1Position] = "1";
+		playerBase[player2Position] = "2";
+		//Sets wall boundaries for battle field
+		playerBase[0] = "|";
+		playerBase[base.length - 1] = "|";
+		//Creating player field
+		for(int index = 1; index < base.length -1 ; index ++)
+		{
+			if(playerBase[index] == "1" || playerBase[index] == "2")
+				playerBase[index] = playerBase[index];
+			else
+				playerBase[index] = " ";
+		}
+	}
+	public void initializePlayerFloor(int position1,int position2)
+	{
+		//Sets player start position where specified
+		player1Position = position1;
+		player2Position = position2;
 		playerBase[player1Position] = "1";
 		playerBase[player2Position] = "2";
 		//Sets wall boundaries for battle field
@@ -92,11 +111,60 @@ public class Battlefield {
 		}
 		updatePlayerField(player1Position,player2Position);
 	}
+	public void moveLeftTwo(int position)
+	{
+		moveLeft(position);
+		if(collisionLeft(position - 1))
+		{
+			moveLeft(position- 1);
+		}
+	}
+	public void moveRightTwo(int position)
+	{
+		moveRight(position);
+		if(collisionRight(position + 1))
+		{
+			moveRight(position + 1);
+		}
+	}	
+	public void moveFront(int movingPlayer, int player)
+	{
+		if((movingPlayer - player) < 0)
+		{
+			playerBase[movingPlayer] = " ";
+			player1Position  = player2Position - 1;
+		}
+		else if((movingPlayer - player) > 0)
+		{
+			playerBase[movingPlayer] = " ";
+			player2Position = player1Position + 1;
+		}
+		updatePlayerField(player1Position,player2Position);
+		
+	}
+	public void pushBack(int player)
+	{
+		if(playerBase[player] == "1")
+		{
+			if(collisionLeft(player))
+			{
+				moveLeft(player);
+				moveLeftTwo(player - 1);
+			}
+		}
+		if(playerBase[player] == "2")
+		{
+			if(collisionRight(player))
+			{
+				moveRight(player);
+				moveRightTwo(player + 1);
+			}
+		}
+	}
 	public boolean collisionLeft(int position)
 	{
 		if(playerBase[position - 1] == " ")
 		{
-			System.out.println("true");
 			return true;
 		}
 		else
@@ -109,7 +177,6 @@ public class Battlefield {
 	{
 		if(playerBase[position + 1] == " ")
 		{
-			System.out.println("true");
 			return true;
 		}
 		else
@@ -122,9 +189,6 @@ public class Battlefield {
 	{
 		return(Math.abs(player2Position - player1Position));
 	}
-	
-	
-	////////TESTING AREA///////////////TESTING AREA//////////////////TESTING AREA///////////////TESTING AREA/////////////
 	public void printFloor()
 	{
 		System.out.println();
@@ -134,14 +198,19 @@ public class Battlefield {
 		}
 		System.out.println();
 	}
-	public void printInitPlayerFloor()
+	public void printPlayerFloor()
 	{
 		for(int index = 0; index < playerBase.length; index ++)
 		{
 			System.out.print(playerBase[index]);	
 		}
 	}
-	
+	public void printBoth()
+	{
+		printPlayerFloor();
+		printFloor();
+	}
+
 	public static void main(String[] args) { 
 //Creating battlefield
 		Battlefield field = new Battlefield(10);
@@ -149,24 +218,38 @@ public class Battlefield {
 		field.initializeFloor();
 		field.initializePlayerFloor();
 //print initial battlefield
-		field.printInitPlayerFloor();
-		field.printFloor();
-//Checking position 10 collision left & right
-		field.collisionLeft(10);
-		field.collisionRight(10);		
-//player two moving to the left 2x
-		field.moveLeft(10);
-		field.moveLeft(10);
-		System.out.println();
-		field.printInitPlayerFloor();
-		field.printFloor();
-//Checking position 1 collision left & right
-		field.collisionLeft(1);
-		field.collisionRight(1);
-		field.collisionRight(9);
-		field.collisionRight(9);
-		System.out.println("Range: " +field.rangeCheck());
-			
+		field.printBoth();
+//using moveLeftTwo function
+		if(field.collisionLeft(field.player2Position))
+			field.moveLeftTwo(field.player2Position);
+		field.printBoth();
+//using moveRightTwo function
+		if(field.collisionRight(field.player1Position))
+			field.moveRightTwo(field.player1Position);
+		field.printBoth();
+//player2 moving in front of player1		
+		field.moveFront(field.player2Position, field.player1Position);
+		field.printBoth();
+//player2 moving two spaces to the left		
+		if(field.collisionLeft(field.player2Position))
+			field.moveLeftTwo(field.player2Position);
+		field.printBoth();
+//player2 pushed back		
+		field.pushBack(field.player2Position);
+		field.printBoth();
+//Player1 moving front of player2		
+		field.moveFront(field.player1Position, field.player2Position);
+		field.printBoth();
+//Player1 being pushed back		
+		field.pushBack(field.player1Position);
+		field.printBoth();
+//Player2 moving rightTwo		
+		field.moveRightTwo(field.player2Position);
+		field.printBoth();
+//Player2 moving rightTwo		
+		field.moveRightTwo(field.player2Position);
+		field.printBoth();
+		
 		}
 	}
 
--- BattlefieldTest.java
+++ BattlefieldTest.java
@@ -51,6 +51,22 @@ public class BattlefieldTest {
 		Assert.assertArrayEquals(expectedOut,field.playerBase);
 	}
 	@Test
+	public void testInitializePlayerFloorSetPosition()
+	{
+		String [] expectedOut = {"|"," "," "," "," ","1","2"," "," "," "," ","|"};
+		Battlefield field = new Battlefield();
+		field.initializePlayerFloor(5,6);
+		Assert.assertArrayEquals(expectedOut,field.playerBase);		
+	}
+	@Test
+	public void testInitializePlayerFloorSetPositionSetSize()
+	{
+		String [] expectedOut = {"|"," "," "," "," ","1"," "," "," "," ","2"," "," "," "," "," ","|"};
+		Battlefield field = new Battlefield(15);
+		field.initializePlayerFloor(5,10);
+		Assert.assertArrayEquals(expectedOut,field.playerBase);
+	}
+	@Test
 	public void testMoveLeft() 
 	{
 		String [] expectedOut = {"|","1"," "," "," "," "," "," "," ","2"," ","|",};
@@ -69,7 +85,49 @@ public class BattlefieldTest {
 		Assert.assertArrayEquals(expectedOut,field.playerBase);
 	}
 	@Test
-	public void testmoveRight()
+	public void testMoveLeftSetSizeSetPosition() 
+	{
+		String [] expectedOut = {"|"," "," "," "," "," "," "," "," "," ","1","2"," "," "," "," "," "," "," "," "," ","|"};
+		Battlefield field = new Battlefield(20);
+		field.initializePlayerFloor(10,11);
+		if(field.collisionLeft(11))
+			field.moveLeft(11);
+		Assert.assertArrayEquals(expectedOut,field.playerBase);
+	}
+	@Test
+	public void testMoveLeftTwo()
+	{
+		String [] expectedOut = {"|","1"," "," "," "," "," "," ","2"," "," ","|",};
+		Battlefield field = new Battlefield();
+		field.initializePlayerFloor();
+		if(field.collisionLeft(10))
+			field.moveLeftTwo(10);
+		Assert.assertArrayEquals(expectedOut,field.playerBase);		
+	}
+	@Test
+	public void testMoveLeftTwoSetSize()
+	{
+		String [] expectedOut = {"|","1"," "," "," "," "," "," "," "," "," ","2"," "," "," "," ","|"};
+		Battlefield field = new Battlefield(15);
+		field.initializePlayerFloor();
+		if(field.collisionLeft(15))
+			field.moveLeftTwo(15);
+		if(field.collisionLeft(13))
+			field.moveLeftTwo(13);
+		Assert.assertArrayEquals(expectedOut,field.playerBase);
+	}
+	@Test
+	public void testMoveLeftTwoSetSizeSetPosition()
+	{
+		String [] expectedOut = {"|","1"," "," ","2"," "," "," "," "," "," "," "," "," "," "," "," "," ","|"};
+		Battlefield field = new Battlefield(17);
+		field.initializePlayerFloor(3,4);
+		if(field.collisionLeft(3))
+			field.moveLeftTwo(3);
+		Assert.assertArrayEquals(expectedOut,field.playerBase);
+	}
+	@Test
+	public void testMoveRight()
 	{
 		String [] expectedOut = {"|"," ","1"," "," "," "," "," "," "," ","2","|",};
 		Battlefield field = new Battlefield();
@@ -87,6 +145,47 @@ public class BattlefieldTest {
 		Assert.assertArrayEquals(expectedOut,field.playerBase);
 	}
 	@Test
+	public void testMoveRightSetSizeSetPosition() 
+	{
+		String [] expectedOut = {"|"," "," "," "," "," "," "," "," "," "," "," ","1"," ","2"," ","|"};
+		Battlefield field = new Battlefield(15);
+		field.initializePlayerFloor(12,13);
+		field.moveRight(13);
+		Assert.assertArrayEquals(expectedOut,field.playerBase);
+	}
+	@Test
+	public void testMoveRightTwo()
+	{
+		String [] expectedOut = {"|"," "," ","1"," "," "," "," "," "," ","2","|",};
+		Battlefield field = new Battlefield();
+		field.initializePlayerFloor();
+		if(field.collisionRight(1))
+			field.moveRightTwo(1);
+		Assert.assertArrayEquals(expectedOut,field.playerBase);
+	}
+	@Test
+	public void testMoveRightTwoSetSize() 
+	{
+		String [] expectedOut = {"|"," "," ","1"," "," "," "," "," "," "," "," "," "," "," ","2","|"};
+		Battlefield field = new Battlefield(15);
+		field.initializePlayerFloor();
+		if(field.collisionRight(1))
+			field.moveRightTwo(1);
+		if(field.collisionRight(15))
+			field.moveRight(15);
+		Assert.assertArrayEquals(expectedOut,field.playerBase);
+	}
+	@Test
+	public void testMoveRightTwoSetSizeSetPosition() 
+	{
+		String [] expectedOut = {"|"," ","1"," "," ","2"," "," "," ","|"};
+		Battlefield field = new Battlefield(8);
+		field.initializePlayerFloor(2,3);
+		if(field.collisionRight(3))
+			field.moveRightTwo(3);
+		Assert.assertArrayEquals(expectedOut,field.playerBase);
+	}
+	@Test
 	public void testCollisionLeft()
 	{
 		Battlefield field = new Battlefield();
@@ -101,6 +200,13 @@ public class BattlefieldTest {
 		assertTrue("No Collision detected",field.collisionLeft(15));
 	}
 	@Test
+	public void testcollisionLeftSetSizeSetPosition()
+	{
+		Battlefield field = new Battlefield(15);
+		field.initializePlayerFloor(12,13);
+		assertTrue("No Collision detected",field.collisionLeft(12));
+	}
+	@Test
 	public void testCollisionRight()
 	{
 		Battlefield field = new Battlefield();
@@ -113,6 +219,67 @@ public class BattlefieldTest {
 		Battlefield field = new Battlefield(15);
 		field.initializePlayerFloor();
 		assertFalse("Collision detected",field.collisionRight(15));	
+	}
+	@Test
+	public void testCollisionRightSetSizeSetPosition()
+	{
+		Battlefield field = new Battlefield(30);
+		field.initializePlayerFloor(25,26);
+		assertFalse("Collision detected",field.collisionRight(25));	
+	}
+	@Test
+	public void testMoveFront()
+	{
+		String [] expectedOut = {"|"," "," "," "," "," "," "," "," ","1","2","|",};
+		Battlefield field = new Battlefield();
+		field.initializePlayerFloor();
+		field.moveFront(1,10);
+		Assert.assertArrayEquals(expectedOut,field.playerBase);	
+	}
+	@Test
+	public void testMoveFrontSetSize()
+	{
+		String [] expectedOut = {"|","1","2"," "," "," "," "," "," "," "," "," "," "," "," "," ","|"};
+		Battlefield field = new Battlefield(15);
+		field.initializePlayerFloor();
+		field.moveFront(15,1);
+		Assert.assertArrayEquals(expectedOut,field.playerBase);
+	}
+	@Test
+	public void testMoveFrontSetSizeSetPosition()
+	{
+		String [] expectedOut = {"|"," ","1","2"," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","|"};
+		Battlefield field = new Battlefield(20);
+		field.initializePlayerFloor(2,19);
+		field.moveFront(19,2);
+		Assert.assertArrayEquals(expectedOut,field.playerBase);
+	}
+	@Test
+	public void testPushBack()
+	{
+		String [] expectedOut = {"|"," "," "," "," ","1"," "," "," ","2"," ","|",};
+		Battlefield field = new Battlefield();
+		field.initializePlayerFloor(5,6);
+		field.pushBack(6);
+		Assert.assertArrayEquals(expectedOut,field.playerBase);	
+	}
+	@Test
+	public void testPushBackSetSize()
+	{
+		String [] expectedOut = {"|","1","2"," "," "," "," "," "," "," "," "," "," "," "," "," ","|"};
+		Battlefield field = new Battlefield(15);
+		field.initializePlayerFloor(1,2);
+		field.pushBack(1);
+		Assert.assertArrayEquals(expectedOut,field.playerBase);
+	}
+	@Test
+	public void testPushBackSetSizeSetPosition()
+	{
+		String [] expectedOut = {"|"," "," "," "," "," "," "," "," "," "," "," "," ","1"," "," "," ","2"," "," "," "," "," "," "," "," ","|"};
+		Battlefield field = new Battlefield(25);
+		field.initializePlayerFloor(16,17);
+		field.pushBack(16);
+		Assert.assertArrayEquals(expectedOut,field.playerBase);
 	}
 	@Test
 	public void testRangeCheck()
